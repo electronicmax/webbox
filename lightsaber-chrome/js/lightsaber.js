6287038467ebf04c3e1324837be75d193669383f
electronic Max <electronic@gmail.com>
2011-10-08 17:35:56 -0400

    got bidirectional communications with contents script

--- lightsaber-chrome/js/lightsaber.js
+++ lightsaber-chrome/js/lightsaber.js
@@ -3,10 +3,22 @@ function _lightsaber() {
         {
             initialize:function(watcher) {
                 var this_ = this;
+                chrome.extension.onConnect.addListener(
+                    function(port) {
+                        port.onMessage.addListener(function(msg) {
+                                                       console.log("on message ", msg);
+                                                       this_.dispatch(port,msg);
+                                                   });
+                    });
                 watcher.bind("changed", function(u) {
                                  console.log("hi oshani! ", u);
                                  this_.changed(u);
                              });
+            },
+            dispatch:function(port,msg) {
+                console.log("something from content script ", port, " and it looked like ", msg);
+                // how we send back is :
+                port.postMessage({question: "hello back"});
             },
             changed:function(u) {
                 // winning starts here.
