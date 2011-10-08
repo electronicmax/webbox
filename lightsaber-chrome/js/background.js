fb95bee7c979e167b688821b43afdd1a6435df7f
electronic Max <electronic@gmail.com>
2011-10-08 16:24:26 -0400

    non crashing plugin framework yaaaaaaaaaaaaaay

--- lightsaber-chrome/js/background.js
+++ lightsaber-chrome/js/background.js
@@ -9,9 +9,8 @@ function _background() {
                 this.bind("change", function() { this_.change.apply(this_, arguments); });
                 chrome.windows.onCreated.addListener(
                     function(window) {
-                        chrome.tabs.getSelected(window, function(tab) {
-                                                    this_.trigger("change", tab.url);
-                                                });
+                        console.log("window >> ", window.id);
+                        chrome.tabs.getSelected(window.id, function(tab) {  this_.trigger("change", tab.url);   });
                     });
                 chrome.windows.onFocusChanged.addListener(
                      function(window) {
@@ -41,31 +40,9 @@ function _background() {
                                                  });                         
                     });
 
-                setInterval(function() {
-                                     if (this_.current_record !== undefined) {
-                                         this_.change(this_.current_record.get("location"));
-                                     }                                     
-                                 }, 200);
-
             },
             change:function(url) {
-               if (this.current_record !== undefined) {
-                   if (url == this.current_record.get("location")) {
-                       this.current_record.set({end:new Date()});
-                       return; 
-                   }
-                   delete this.current_record;
-                   this.trigger("new_record", this.current_record);
-               }
-               if (url !== undefined) {
-                   var now = new Date();
-                   this.current_record = this.make_record({start: now, end:now, to: url, location: url});
-                   this.data.add(this.current_record);                   
-               }
-            },
-            make_record:function(options) {
-                if (options.id === undefined) { options.id = util.guid(); }
-                return new Activity(options);
+                console.log("Page changed ", url);               
             }
         }
     );
@@ -73,11 +50,7 @@ function _background() {
         background : {
             init:function() {
                 this.watcher = new WindowWatcher();
-            },
-            get_streams:function() {
-                return [this.watcher.data];                
             }
-        },
-        Record:Record
+        }
     };        
 };
