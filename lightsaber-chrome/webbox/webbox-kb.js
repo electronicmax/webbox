f3218fc52d4dce87c6864c1c8f536aa0219ad9e4
electronic Max <electronic@gmail.com>
2011-12-11 03:50:19 +0000

    finished get_sp_object call

--- lightsaber-chrome/webbox/webbox-kb.js
+++ lightsaber-chrome/webbox/webbox-kb.js
@@ -1,9 +1,11 @@
 
 define(['/webbox/webbox-ns.js', '/webbox/webbox-config.js' ],
-       function(ns, config) {	   
+       function(ns, config) {
+	   
 	   var make_kb = function() {
 	       return $.rdf.databank([], {base: ns.base, namespaces:ns.ns});
 	   };
+	   
 	   var make_spo_query = function(query, cont) {
 		var get = $.ajax({ type:"GET", url:config.ENDPOINT+"/sparql/", data:{query:query}});
 		var kb = make_kb();
@@ -20,23 +22,34 @@ define(['/webbox/webbox-ns.js', '/webbox/webbox-config.js' ],
 			     if(cont) { cont(results); }
 			 });
 	   };
+	   
 	   var get_sp_object = function(subject, predicate, graph) {
+	       // subject should be a uri,
+	       subject = subject.toString();
+	       predicate = predicate.toString();
 	       var d = new $.Deferred();
-	       var q = graph ?
-		   _.template("SELECT ?o WHERE { GRAPH \<<%= g %>\> { \<<%=s%>\> \<<%=p%>\> ?o . }} LIMIT 100000")({s:subject,p:predicate, g:graph}) :
-	       _.template("SELECT ?o WHERE { \<<%=s%>\> \<<%=p%>\> ?o . } LIMIT 100000").template({s:subject,p:predicate});
-	       console.log("get_value query ", query);
+	       var query = graph ?
+		   _("SELECT ?o WHERE { GRAPH \<<%= g %>\> { \<<%=s%>\> \<<%=p%>\> ?o . }} LIMIT 100000").template({s:subject,p:predicate, g:graph}) :
+	       _("SELECT ?o WHERE { \<<%=s%>\> \<<%=p%>\> ?o . } LIMIT 100000").template({s:subject,p:predicate});
+	       console.log("get_value query ", query, config.ENDPOINT);
 	       var get = $.ajax({ type:"GET", url:config.ENDPOINT+"/sparql/", data:{query:query}}).then(
 		   function(doc) {
-		       window.doc = doc;
-		       console.log(" Okay, party ", doc);
-		   });	       
+		       var lits = $(doc, "results").find('literal');
+		       if (lits.length > 0) {
+			   d.resolve(lits.map(function() { return $(this).text(); }));
+			   return;
+		       }
+		       var uris = $(doc, "results").find('uri');
+		       if (uris.length > 0) {
+			   d.resolve(uris.map(function() { return $(this).text(); }));
+		       }		       
+		   });
+	       return d.promise();	       
 	   };	   
 	   
 	   var get_graphs = function(cont) {
 	       // gets list of distinct ORM entities in the KB:
-	       // TODO: make this more efficient so that we don't have to parse all the triples
-	       
+	       // TODO: make this more efficient so that we don't have to parse all the triples	       
 	       var query = "CONSTRUCT { ?g a <http://webbox.ecs.soton.ac.uk/webbox/Object> } WHERE { GRAPH ?g { ?s ?p ?o. } } LIMIT 100000";
 	       var get = $.ajax({ type:"GET", url:config.ENDPOINT+"/sparql/", data:{query:query}});
 	       var kb = make_kb();
@@ -50,7 +63,8 @@ define(['/webbox/webbox-ns.js', '/webbox/webbox-config.js' ],
 				});
 			    if(cont) { cont(gs); }
 			});
-	   };	   
+	   };
+	   
 	   return {
 	       make_kb:make_kb,
 	       get_graphs:get_graphs,
