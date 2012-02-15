a2215032af3262f32dcf8cb9bd88f943ba52a4fb
electronic Max <electronic@gmail.com>
2012-02-15 10:25:10 -0800

    replaced with old backbone

--- lightsaber-chrome/ui/book/book.js
+++ lightsaber-chrome/ui/book/book.js
@@ -1,4 +1,16 @@
 define([],
       function() {
-	  $('#magazine').turn({page:1, shaows:true, acceleration:true});	  
+	  $('#magazine').bind('turned', function(e, page) {
+				  console.log('Current view: ', $('#magazine').turn('view'));
+			      });
+	  $('#magazine').turn({page:1, shaows:true, acceleration:true});
+	  $('#next').bind('click', function() { $('#magazine').turn('next'); });
+	  $('#prev').bind('click',  function() {  $('#magazine').turn('previous'); });
+	  // make some sleep charts
+	  $.plot($("#sleepgraph"),
+		 [{
+                     data: [[0,1],[1,2],[2,6],[3,3],[4,4]],
+                     bars:{ show:true }
+                 }]);
+	  $('#magazine').turn('next'); 	  
       });
